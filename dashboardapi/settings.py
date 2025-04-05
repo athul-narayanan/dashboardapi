a0fba33e1a1535c26c88fa772ab0cd51f80fb926
athul-narayanan <athulnarayanan62@gmail.com>
2025-04-04 21:06:20 -0400

    CORS ADDED

--- dashboardapi/settings.py
+++ dashboardapi/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'dashboard'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+    "https://dashboard-new-6wpg.vercel.app/"
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
